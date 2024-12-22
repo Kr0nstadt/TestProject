f2def99f827bd4788c0a0783e451f2c56816a82e
Карпова Яна Алексеевна <ip316s08@csc.sibsutis.ru>
2024-12-22 16:25:47 +0700

    Интерфейс для полной статистики

---
+++ Progect/src/main/java/org/example/IFillStatistic.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import java.util.Spliterator;
+
+public interface IFillStatistic {
+    public void GetMin();
+    public void GetMax();
+    public void GetSum();
+    public void GetMid();
+    public void GetSize();
+}
