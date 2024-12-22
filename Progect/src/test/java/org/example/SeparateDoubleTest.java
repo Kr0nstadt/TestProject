eb95b19e0f3e49807e0bbcd1c462b82a9014e642
Карпова Яна Алексеевна <ip316s08@csc.sibsutis.ru>
2024-12-22 13:06:27 +0700

    Тесты для вещественых

---
+++ Progect/src/test/java/org/example/SeparateDoubleTest.java
@@ -0,0 +1,107 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SeparateDoubleTest {
+    @Test
+    public void TestSeparateDoubleIsMatchNull(){
+        String txt = null;
+        SeparateDouble separate = new SeparateDouble();
+
+        boolean res = separate.IsMath(txt);
+
+        assertFalse(res);
+    }
+    @Test
+    public void TestSeparateDoubleIsMatchEmpty(){
+        String txt = "";
+        SeparateDouble separate = new SeparateDouble();
+
+        boolean res = separate.IsMath(txt);
+
+        assertFalse(res);
+    }
+    @Test
+    public void TestSeparateDoubleIsMatch1(){
+        String txt = "1.73";
+        SeparateDouble separate = new SeparateDouble();
+
+        boolean res = separate.IsMath(txt);
+
+        assertTrue(res);
+    }
+    @Test
+    public void TestSeparateDoubleIsMatch2(){
+        String txt = "-0.5";
+        SeparateDouble separate = new SeparateDouble();
+
+        boolean res = separate.IsMath(txt);
+
+        assertTrue(res);
+    }
+    @Test
+    public void TestSeparateDoubleIsMatch3(){
+        String txt = "+3.14E10";
+        SeparateDouble separate = new SeparateDouble();
+
+        boolean res = separate.IsMath(txt);
+
+        assertTrue(res);
+    }
+    @Test
+    public void TestSeparateDoubleIsMatch4(){
+        String txt = "2.71828e-5";
+        SeparateDouble separate = new SeparateDouble();
+
+        boolean res = separate.IsMath(txt);
+
+        assertTrue(res);
+    }
+    @Test
+    public void TestSeparateDoubleIsMatch5(){
+        String txt = "1.0E0";
+        SeparateDouble separate = new SeparateDouble();
+
+        boolean res = separate.IsMath(txt);
+
+        assertTrue(res);
+    }
+    @Test
+    public void TestSeparateDoubleIsMatch6(){
+        String txt = "123.";
+        SeparateDouble separate = new SeparateDouble();
+
+        boolean res = separate.IsMath(txt);
+
+        assertFalse(res);
+    }
+    @Test
+    public void TestSeparateDoubleIsMatch7(){
+        String txt = ".456";
+        SeparateDouble separate = new SeparateDouble();
+
+        boolean res = separate.IsMath(txt);
+
+        assertTrue(res);
+    }
+    @Test
+    public void TestSeparateDoubleIsMatch8(){
+        String txt = "abc";
+        SeparateDouble separate = new SeparateDouble();
+
+        boolean res = separate.IsMath(txt);
+
+        assertFalse(res);
+    }
+    @Test
+    public void TestSeparateDoubleIsMatch9(){
+        String txt = "1.73E12";
+        SeparateDouble separate = new SeparateDouble();
+
+        boolean res = separate.IsMath(txt);
+
+        assertTrue(res);
+    }
+}
