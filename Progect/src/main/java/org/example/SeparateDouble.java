fb03279ccb38f403fe60d28146f3daf0d167a04e
Карпова Яна Алексеевна <ip316s08@csc.sibsutis.ru>
2024-12-22 12:49:27 +0700

    Создание класса для вещественных чисел

---
+++ Progect/src/main/java/org/example/SeparateDouble.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import java.util.regex.Pattern;
+
+public class SeparateDouble implements ICanSeparate<Double>{
+    @Override
+    public boolean IsMath(String txt) {
+        if(txt == null||
+                txt.isEmpty()||
+                txt.length() == 0){
+            return false;
+        }
+        else{
+            return Pattern.matches("^[-+]?\\d*\\.\\d+([eE][-+]?\\d+)?$",txt);
+        }
+    }
+
+    @Override
+    public Double getValue(String txt) {
+        Double res = 0.0;
+        try{
+            res = Double.parseDouble(txt);
+        }
+        catch (NumberFormatException e){}
+        return res;
+    }
+}
